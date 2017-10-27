93b866df76f290060eb0e7acf408ce2bbd247504
M3ttle <nonni@nocando.is>
2017-10-27 01:04:26 +0000

    failed test

--- src/test/java/is/ru/hugb/calc/StringCalculatorTest.java
+++ src/test/java/is/ru/hugb/calc/StringCalculatorTest.java
@@ -31,6 +31,6 @@ public class StringCalculatorTest {
     @Test
     public void testWithNewLineDelimiter() {
         StringCalculator Calculator = new StringCalculator();
-        assertEquals(6, Calculator.add("1\n2,3"));
+        assertEquals(5, Calculator.add("1\n2,3"));
     }
 }
