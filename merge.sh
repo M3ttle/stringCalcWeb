da3fb7f6e8859250d585b05f65c502bdae3185f9
M3ttle <nonni@nocando.is>
2017-10-28 22:31:42 +0000

    asrfasdf

--- merge.sh
+++ merge.sh
@@ -14,8 +14,9 @@ TO_BRANCH="master"
 
 # Get the current branch
 export PAGER=cat
-CURRENT_BRANCH=$(git log -n 1 --pretty=%d HEAD | cut -d"," -f3 | cut -d" " -f2 | cut -d")" -f1)
-echo "current branch is '$CURRENT_BRANCH'"
+#CURRENT_BRANCH=$(git log -n 1 --pretty=%d HEAD | cut -d"," -f3 | cut -d" " -f2 | cut -d")" -f1)
+#echo "current branch is '$CURRENT_BRANCH'"
+CURRENT_BRANCH="dev"
 
 # Create the URL to push merge to 
 URL=$(git remote -v | head -n1 | cut -f2 | cut -d" " -f1)
